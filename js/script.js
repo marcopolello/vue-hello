f13526c04b56df76059666b57bc998adda993f33
marcopolello <marco-pol@hotmail.it>
2020-11-17 10:56:08 +0100

    usato metodo vue per fare toggle img

--- js/script.js
+++ js/script.js
@@ -2,17 +2,25 @@
 // Bonus: Aggiungere un’immagine presa anch’essa da un data
 // Altro possibile Bonus:
 // gestire una funzione con un evento nel modo Vue.
-var variabile = prompt("scrivi qualcosa");
+
+// var variabile = prompt("scrivi qualcosa");
 var app = new Vue({
   el: '#app',
   data: {
     message: "stampa a schermo un messaggio",
-    text: variabile,
+    text: "del testo",
     picture: "https://cdn.pixabay.com/photo/2020/11/09/18/54/mountains-5727541__340.jpg",
+    myClass: "invisible",
   },
   methods: {
-    toggleImg: function() {
-      document.getElementById('immagine').classList.toggle('invisible');
-    }
+    toggleImg: function() { this.myClass === "invisible" ? this.myClass = "visible" : this.myClass = "invisible"}
+      //document.getElementById('immagine').classList.toggle('invisible')   versione precedente.
+     //  if (this.myClass === "invisible"){
+     //   this.myClass = "visible";
+     // } else {
+     //   this.myClass = "invisible";
+     // }
+
+    //}
   }
 });
